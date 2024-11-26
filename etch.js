65177b422f7352f74a6682d53350c23171803c8a
Marouane <184505120+marj-ayy@users.noreply.github.com>
2024-11-26 20:54:52 +0100

    Add scalable grids

--- etch.js
+++ etch.js
@@ -1,25 +1,36 @@
-const numberBoxes = 16;
-const containerBox = document.querySelector(".container");
-for(let j = 1; j <= numberBoxes; ++j){
-    const rowBox = document.createElement("div");
-    rowBox.classList.add("rowbox");
-    for(let i = 1; i <= numberBoxes; ++i){
-        const box = document.createElement("div");
-        box.classList.add("box");
-        rowBox.appendChild(box);
+function drawGrid(numberBoxes){
+    if(numberBoxes <= 100){
+    const containerBox = document.querySelector(".container");
+    for(let j = 1; j <= numberBoxes; ++j){
+        const rowBox = document.createElement("div");
+        rowBox.classList.add("rowbox");
+        for(let i = 1; i <= numberBoxes; ++i){
+            const box = document.createElement("div");
+            box.classList.add("box");
+            rowBox.appendChild(box);
+        }
+        containerBox.appendChild(rowBox);
+    }   
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => box.addEventListener("mouseover", (e) => {
+        let red = Math.round(Math.random() * 255);
+        let green = Math.round(Math.random() * 255);
+        let blue = Math.round(Math.random() * 255);
+        e.target.style["background-color"] = `rgb(${red}, ${green}, ${blue})`;
+    }));
+    const resetButton = document.querySelector(".reset");
+    resetButton.addEventListener("click", (e) => {
+        boxes.forEach((box) => box.style["background-color"] = '');
+    });
     }
-    containerBox.appendChild(rowBox);
-}   
+    else
+        alert("You have inputed a number of boxes superior to 100. Please try again with a different valid number .");
+}
 
-const boxes = document.querySelectorAll(".box");
-boxes.forEach((box) => box.addEventListener("mouseover", (e) => {
-    let red = Math.round(Math.random() * 255);
-    let green = Math.round(Math.random() * 255);
-    let blue = Math.round(Math.random() * 255);
-    e.target.style["background-color"] = `rgb(${red}, ${green}, ${blue})`;
-}));
-
-const resetButton = document.querySelector(".reset");
-resetButton.addEventListener("click", (e) => {
-    boxes.forEach((box) => box.style["background-color"] = '')
+const newGridButton = document.querySelector(".newGrid");
+newGridButton.addEventListener("click", (e) => {
+    let numberBoxes = parseInt(prompt("How many boxes do you want your new grid to be ( Maximum 100 ) ? "));
+    const rowBoxes = document.querySelectorAll(".rowbox");
+    rowBoxes.forEach((rowbox) => rowbox.remove());
+    drawGrid(numberBoxes);
 });
